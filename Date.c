75cf04ae5f535d1c33bf918e1341b671488516b2
shashi-06-03 <109144797+shashi-06-03@users.noreply.github.com>
2022-07-18 17:18:01 +0530

    Add files via upload

---
+++ Date.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int day, month, year;
+    printf("Enter Date in DD/MM/YYYY format\n");
+    scanf("%d/%d/%d",&day,&month,&year);
+    printf("Day-%d,Month-%d,Year-%d",day,month,year);
+    return 0;
+}
---
+++ Time.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int main()
+ {
+     int hour, minute;
+     printf("Enter Date in Format HH:MM");
+     scanf("%d:%d",&hour,&minute);
+     printf("%d Hour and %d Minute",hour,minute);
+     return 0;
+
+ }
