cc221eaa85400c9f1ab9835941b9f337e12cc43f
shashi-06-03 <109144797+shashi-06-03@users.noreply.github.com>
2022-07-18 16:31:45 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main()
+    {
+
+
+    float r,a=0;
+    printf("Enter the radius of circle");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("Area of CIRCLE=%f having the radius %f",a,r);
+    return 0;
+     }
---
+++ length of the string.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+
+    int x=printf("Shashikant");
+    printf("\n");
+    printf("%d",x);
+    printf("\n");
+}
